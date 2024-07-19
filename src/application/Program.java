d3e4c4d322f47b3182382830ac4efee39a1b6e0c
Jose Everton <devjoseeverton@gmail.com>
2024-07-19 09:24:50 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,20 @@
 package application;
 
+import java.time.LocalDate;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Department obj = new Department(1, "books");
+		
+		Seller seller = new Seller(21, "bob", "bob@mail.com" , LocalDate.parse("1997-05-31"), 3000.0, obj);
+		
+		System.out.println(seller);
+		
 	}
 
 }
